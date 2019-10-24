6376c65641c53442396c0d9486a3798c91c97de3
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-10-24 09:36:46 +0900

    CREATE:buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="RightBody__first">
+                  <div class="RightBody__title">
+                    <div class="RightBody__name">
+                      
+                    </div>
+                    <div class="RightBody__date">
+                      2019/10/24 00:15
+                    </div>
+                  </div>
+                  <div class="RightBody__text">
+                    <p class="RightBody__content">
+                    aaa
+                    </p>
+                  </div>
+                </div>`
+    return html;
+  }
+
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,11 +30,11 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(){
-
+    .done(function(data){
+      var html = buildHTML(data);
     })
     .fail(function(){
-      
+
     })
   });
 });
