ce151ceb32377977360ee5f86652940c90481c9e
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-10-25 19:29:20 +0900

    ADD : Append

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,26 @@
 $(function(){
+
+  var search_list = $('.chat-group-form__input')
+
+  function appendUser(user) {
+    var html = `
+            <div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">${user.name}</p>
+              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+            </div>
+            `
+    search_list.append(html)
+  }
+
+  function appendErrMsgToHTML(msg) {
+    var html = `
+               <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${msg}</p>
+               </div>`
+    search_list.append(html)
+  }
+
+
   function buildHTML(message){
     var html = `<div class="RightBody__first">
                   <div class="RightBody__title">
@@ -57,5 +79,17 @@ $(function(){
       data: { keyword : input },
       dataType: 'json'
     })
+    .done(function(users){
+      search_list.empty();
+      if(users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+          debugger
+        });
+      } else {
+        appendErrMsgToHTML("ユーザーが見つかりません");
+      }
+    })
+    
   });
 });
