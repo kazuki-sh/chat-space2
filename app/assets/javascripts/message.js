e0822fc362422490635aa2be12da0685f9f2161a
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-10-26 11:57:56 +0900

    ADD : MemberAddFunction

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,5 @@
 $(function(){
 
-  var search_list = $('#user-search-result')
-
-  function appendUser(user) {
-    var html = `
-              <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-              </div>
-              `
-    console.log(html);
-    search_list.append(html);
-  }
-
-  function appendErrMsgToHTML(msg) {
-    var html = `
-               <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${msg}</p>
-               </div>
-               `
-    search_list.append(html);
-  }
-
-
   function buildHTML(message){
     var html = `<div class="RightBody__first">
                   <div class="RightBody__title">
@@ -70,29 +47,6 @@ $(function(){
     })
     .fail(function(){
       alert('error');
-    })
-  });
-
-  $('#user-search-field').on('keyup', function(){
-    var input = $('#user-search-field').val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword : input },
-      dataType: 'json'
-    })
-    .done(function(users){
-      search_list.empty();
-      if(users.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      } else {
-        appendErrMsgToHTML("ユーザーが見つかりません");
-      }
-    })
-    .fail(function(){
-      alert("ユーザー検索に失敗しました。")
     })
   });
 });
